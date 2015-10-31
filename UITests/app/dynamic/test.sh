e456dded846c320c594f32664999bc3d0a1fadf6
ArcanaMagus <ArcanaMagus@users.no-reply.github.com>
2015-10-31 18:52:01 +0000

    Add newfile app/dynamic/test.sh

---
+++ UITests/app/dynamic/test.sh
@@ -0,0 +1,209 @@
+ls -a
+git status -sb
+git checkout -b param
+git checkout -b param origin param
+git checkout -b init
+git checkout -b init origin init
+git checkout -b array
+git checkout -b array origin array
+git checkout -b index
+git checkout -b index origin index
+git checkout -b config
+git checkout -b config origin config
+#git checkout -b debugger
+#git checkout -b debugger origin debugger
+#git checkout -b debug
+#git checkout -b debug origin debug
+#git checkout -b ArcanaMagus-debugger
+#git checkout -b ArcanaMagus-debugger origin ArcanaMagus-debugger
+#git checkout -b readme
+#git checkout -b readme origin readme
+git checkout -b userAuth.cpl
+git checkout -b userAuth.cpl origin userAuth.cpl
+git checkout -b userAuth.api
+git checkout -b userAuth.api origin userAuth.api
+git checkout -b userAuth.app
+git checkout -b userAUth.app origin userAauth.app
+git checkout -b api
+git checkout -b api origin api
+git checkout -b build
+git checkout -b build origin build
+git checkout -b binary
+git checkout -b binary orgin binary
+git checkout -b project
+git checkout -b project origin project
+git checkout -b package
+git checkout -b pacakge origin package
+git checkout -b userAuth
+git checkout -b userAuth origin userAuth
+git checkout -b userAuth.UITests
+git checkout -b userAuth.UITests origin userAuth.UITests
+#git checkout -b Readme
+#git checkout -b Readme origin Readme
+#git checkout -b Readme-1
+#git checkout -b origin Readme-1
+git checkout -b xamarin-build
+git checkout -b xamarin-build origin xamarin-build
+git checkout -b master
+git checkout -b master origin master
+
+git stage app/dynamic/test.sh
+git add app/dynamic/test.sh
+cd app/dynamic/
+.\test.sh
+cd C:/Users/RonThomas/Source/Repos/userAuthenticate/userAuth/UITests/
+git commit -m "Add newfile app/dynamic/test.sh"
+git push origin HEAD:master
+git pull origin master
+sleep 10
+git push origin HEAD:array
+git pull origin array
+git push origin HEAD:init
+git pull origin init
+git push origin HEAD:param
+git pull origin param
+#git push origin HEAD:browser
+#git pull origin browser
+sleep 14
+git push origin HEAD:index
+git pull origin index
+sleep 12
+git push origin HEAD:config
+git pull origin config
+#sleep 10
+#git push origin HEAD:Grunt
+#git pull origin Grunt
+#sleep 5
+#git push origin HEAD:JSON
+#git pull origin JSON
+#sleep 10
+#git push origin HEAD:HTML
+#git pull origin HTML
+#sleep 22
+git push origin HEAD:debugger
+git pull origin debugger
+sleep 13
+git push origin HEAD:debug
+git pull origin debug
+sleep 15
+git push origin HEAD:ArcanaMagus-debugger
+git pull orgin ArcanaMagus-debugger
+sleep 7
+git push origin HEAD:readme
+git pull origin readme
+sleep 12
+git push origin HEAD:userAuth.api
+git pull origin userAuth.api
+sleep 14
+git push origin HEAD:userAuth.app
+git pull origin userAuth.app
+sleep 5
+git push origin HEAD:userAuth.cpl
+git pull origin userAuth.cpl
+sleep 5
+git push origin HEAD:api
+git pull origin api
+sleep 11
+git push origin HEAD:build
+git pull origin build
+sleep 14
+git push origin HEAD:binary
+git pull origin binary
+sleep 12
+git push origin HEAD:xamarin-build
+git pull origin xamarin-build
+#git push origin HEAD:Mocha
+#git pull origin Mocha
+#sleep 13
+git push origin HEAD:project
+git pull origin project
+#sleep 10
+#git push orgin HEAD:javascript
+#git pull origin javascript
+#sleep 6
+git push origin HEAD:package
+git pull origin package
+sleep 8
+git push origin userAuth
+git push origin HEAD:userAuth
+git pull origin userAuth
+#git push origin HEAD:ArcanaMagus-HTML
+#git pull orgin ArcanaMagus-HTML
+git push origin HEAD:Readme
+git pull origin Readme
+git push origin HEAD:Readme-1
+git pull origin Readme-1
+git diff -b
+git status -sb
+git checkout master
+git pull https://github.com/ArcanaMagus/userAuth.cpl.git
+#git merge --no-ff
+dos2unix test.shtest.sh
+for {
+app/dynamic/test.sh= var obj
+if (obj is unix) {
+unix2mac test.shtest.sh
+}
+else (obj is mac) {
+null;
+	}
+}
+.\build
+test.completion = function (cmds, op) {
+var sys = cmds.conf.sys.complete
+if (sys[1] !== "test") sys.shuffle("test"),
+if (sys.length === 3) {}
+if (sys.width === 3) {}
+if (sys.height === 3) {}
+def: widget
+	var changed = function (const, internal, op)
+	test.changed.eval = const(array: internal, Boolean: wdca, eval: true, value="none")
+		if (const: array[1]) {
+		intdph.conf.array.changed = Int32(Array in widget)
+
+		}
+
+	var widget = function (click, ui)
+		for
+		 { ui = sys[3]( const: "click" eval: true, test: false)
+		 	 , Reader reader = rdr
+			 , Int32 initialDepth = intdph
+			 , Boolean writeChilren = wrtch
+			 , Boolean writeDataConstructorAs = wdca
+		 }
+}
+, else  {
+bash  - start app/dynamic/test.sh
+}
+#condition
+if (diff >= 1) {
+const: eval: frame(in).shell
+}fi
+#condition2
+if (diff ==< 1) {
+while (diff != 0);
+	conset: eval: frame(locationAt(Error): void) in cmd
+}fi
+while { const: err().args }
+for {
+var location = System.Cli.Pointer.Value(X, Y, Z)
+{
+X = (location.length);
+
+Y = (location.width);
+
+Z = (location.height);
+
+}
+case: "init"
+case: "err"
+case: "WFSO"
+	var init = console.print(err at)
+   , err = static.frame_exception(meta::init)
+   , WFSO = userauth.Timeout.console.Operand(delay="none")
+   , Timeout Exception texp = userauth.System.IO.Exception(frame: => int)
+console.err(args) args="none"; frame_exception(in console::meta)
+case: "init":  return init(err:location[X((0), X(1), Y(0), Z(0))])
+case: "err": return err(init: location[Y((1), Y(0), Z(0))])
+case: "WFSO": return WFSO( console.print(stat) + dat.location() void
+	}
