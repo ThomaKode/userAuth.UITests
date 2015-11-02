68dfa4151e71b02605d49d1bfcfe6daaf846c8f0
ArcanaMagus <ArcanaMagus@users.no-reply.github.com>
2015-11-02 00:55:55 +0000

    Modify file C:/Users/RonThomas/Source/Repos/userAuthenticate/userAuth/UITests/Tests.cs

---
+++ UITests/Tests.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using NUnit.Framework;
+using Xamarin.UITest;
+using Xamarin.UITest.Android;
+using Xamarin.UITest.Queries;
+
+namespace userAuth.UITests
+{
+	[TestFixture]
+	public class Tests
+	{
+		AndroidApp app;
+
+		[SetUp]
+		public void BeforeEachTest ()
+		{
+			app = ConfigureApp.Android.StartApp ();
+		}
+
+		[Test]
+		public void WelcomeTextIsDisplayed ()
+		{
+			AppResult[] results = app.WaitForElement (c => c.Marked ("Welcome to Xamarin Forms!"));
+			app.Screenshot ("Welcome screen.");
+
+			Assert.IsTrue (results.Any ());
+		}
+	}
+	[initMethod]
+	public class initUI: Object
+	{
+		ExceptionFrame frame;
+
+		[Pre]
+		public void PreTest ()
+		{
+			frame = SyncFrame.GUI.CountFrame ();
+			object ApplicationException::meta
+		}
+
+
+		[PostTest]
+		public void PostTestFrameIsCounted ()
+		{
+			FrameCount[] counts = frame.RecordForEachElement ( e => e.Logged ("Frame Element Counted"));
+			frame.CmdLog (counts 1+= {frame: eval)) - counts (var i = 25000));
+
+				Math.IsInstance (counts.ToString ());
+		}
+				}	private exception postestframeisdropped ()
+				{	frame.exception = AssemblyLoadEventHandler () {initUI.Equals(frame:ref)
+						while (ref:frame) is sbyte.length(>=1)
+							sim.console(init):
+						}
+						/*frmstr === frame strength/quality.
+							e.g( frmstr[100] == optimal;
+								while(frmstr[1] == minimum;
+								*/
+						public dynamic void frmstr (stat, frmstr)
+					{
+						var frmstr = Func(frame, op)
+						ConsoleColor hex(#0xefaa613a3);
+					var column = Attribute(Element).function(frame, EventArgs: ThreadStaticAttribute(maxThreads=4): StackOrientation: vertical)
+						, row = Attribute(Element).function(frame, EventArgs: ThreadStaticAttribute(maxThreads=4): StackOrientation: horizontal)
+						, grid = userAuth.Tests.GetType(row + "size")
+
+					for (ushort: DisplayElement) function( default: args="none", head="meta::Attribute", fragment=1),
+					}		{
+					frame (ContextStaticAttribute) int static (DisplayMode: Full){ Func(
+						sbyte: x''
+						, if (const: Meta::eval.Operand): <= Value
+							var Operand = System.ValueType( GetType"boolean or vector" )
+							.
+							return (System.userAuth(frame[1])_AppDomain ( :	Int32 => .Tests_AppDomain (eval=true))			
+								}
+
+				}
+
+}
