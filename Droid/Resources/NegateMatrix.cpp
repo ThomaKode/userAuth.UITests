4817b7d641efbabb59f54aabbb82807c59379753
ArcanaMagus <ArcanaMagus@users.no-reply.github.com>
2015-11-03 07:36:37 +0000

    Modify file C:/Users/RonThomas/Source/Repos/userAuthenticate/userAuth/Droid/Resources/NegateMatrix.cpp

---
+++ Droid/Resources/NegateMatrix.cpp
@@ -0,0 +1,23 @@
+/*
+ * NegateMatrix.cpp
+ *
+ *  Created on: Nov 1, 2015
+ *      Author: RonThomas
+ */
+#include<iostream>
+
+using namespace std;
+int main()
+{
+	int m, n, c, d, first[10][10], second[10][10], sub[10][10];
+	cout << "Matrix height and length in Rows and Columns : ";
+	cin >> m >> n;
+	cout << "Enter the parameters of the first matrix : \n";
+
+	for ( i = 0 ; i < m ; i++ )
+		for ( )
+
+}
+
+
+
