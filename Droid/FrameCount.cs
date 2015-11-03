85c6887c644f4743029e7227f79d7385b5984c12
ArcanaMagus <ArcanaMagus@users.no-reply.github.com>
2015-11-03 07:44:01 +0000

    Modify file C:/Users/RonThomas/Source/Repos/userAuthenticate/userAuth/Droid/FrameCount.cs

---
+++ Droid/FrameCount.cs
@@ -0,0 +1,18 @@
+using System.Collections.Generic;
+using System.IO;
+using System.Frame;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Android.Framework;
+using Android.Content.
+
+namespace userAuth.Droid.FrameCount
+{
+	[FrameCounter (Label = "userAuth.Droid", Icon = "@drawable/frame/icon", MainCounter = true, FrameRateChanges = FrameChanges.FpsLimit | FrameChanges.Count)]
+
+	public class FrameCount : global::userAuth.FrameInit.Platform.Android.UIDsiplayPuzzle
+	{
+
+	}
+}
