b79f945ba37bf768fc68672d8bdf381e0b0a7d9f
ArcanaMagus <ArcanaMagus@users.no-reply.github.com>
2015-12-06 02:29:38 -0400

    Modify File Droid/Protoneq.c

--- Droid/Protoneq.c
+++ Droid/Protoneq.c
@@ -1,71 +1,1 @@
-//Proton Collision Frequency Program 1.0.16
-
-#include <stdio.h>
-#include <mathematica>
-
-#define growth 100 // i.e. Growth rate of 100 
-#define total 512 // i.e. Total collisions is 512
-#define Rate  1000s // i.e. Rate of collision is 1000 collisions per second
-#define Velocity 1+kMNu // Velocity is (+1)Lightspeed constant
-#define direct 1
-#define glance 0
-
-
-namespace userAuth.UITests.Droid 
-{
-typedef char FractConst
-typedef string  bool:
-- direct
-- glance
-typedef int bool 
-	Statconst: *e;
-	
-	char.bind(*E) = { new ElementAt(cursor),
-	type="Image"
-	}
-	public static void Operand
-	{
-		GetHttpUri = builtin[httpRequest], (E, request);
-		ParseHttpUri = builtin[httpResponse], (E, response);
-		
-		ApixServer = E"http://arcnanotech.visualstudio.com",
-		MetaHeader = E"http://arc-tech.visualstudio.com",
-		
-class Protoneq () {
- 		public Protoneq () {
-			 const K = object.MoveSpeed(int) -[MoveVelocity];
-		 	 const kMNu WriteSpeed = E"Write\\";
-			   () {
-				  E"Meta::stat",
-				  E"display @icon.png\/";
-			  }	
-		private interface Meta ()
-		FractConst *E;
-		property stat(E, Meta)
-		{
-			stat::Meta(*E);
-		}
-		property statSustain()
-		return Meta;
-		}
-		enum property _init_stat {
-			from ApixServer(import):
-			function X = {
-				httpGetUriRequest(ApixServer[ flags="none", tags=''E''],
-				__init__ x = setUri.charBind(''Y''),
-				_stat_cpu mod_x = builtin(int[propertyFlags="static Operand", labels="void", ])
-				, include: "ApixServer" in nodeGroup) var nodeGroup = meta.const(initFlag[1])
-				, "MetaHeader" in nodeGroup[1] == statOperand[GroupTo-->stat];
-				
-			}
-		} 
-	 } 
-}
-using namespace userAuth.UITests.Droid.Protoneq.smash
-{			 
-#define charBind
-			 {
-				 private enum 
-			 }
-	}
-}
+//Proton Collision Frequency Program 1.0.16#include <stdio.h>#include <mathematica>#define growth 100 // i.e. Growth rate of 100 #define total 512 // i.e. Total collisions is 512#define Rate  1000s // i.e. Rate of collision is 1000 collisions per second#define Velocity 1+kMNu // Velocity is (+1)Lightspeed constant#define direct 1#define glance 0namespace userAuth.UITests.Droid {typedef char FractConsttypedef string  bool:- direct- glancetypedef int bool 	Statconst: *e;		char.bind(*E) = { new ElementAt(cursor),	type="Image"	}	public static void Operand	{		GetHttpUri = builtin[httpRequest], (E, request);		ParseHttpUri = builtin[httpResponse], (E, response);				ApixServer = E"http://arcnanotech.visualstudio.com",		MetaHeader = E"http://arc-tech.visualstudio.com",		class Protoneq () { 		public Protoneq () {			 const K = object.MoveSpeed(int) -[MoveVelocity];		 	 const kMNu WriteSpeed = E"Write\\";			   () {				  E"Meta::stat",				  E"display @icon.png\/";			  }			private interface Meta ()		FractConst *E;		property stat(E, Meta)		{			stat::Meta(*E);		}		property statSustain()		return Meta;		}		enum property _init_stat {			from ApixServer(import):			function X = {				httpGetUriRequest(ApixServer[ flags="none", tags=''E''],				__init__ x = setUri.charBind(''Y''),				_stat_cpu mod_x = builtin(int[propertyFlags="static Operand", labels="void", ])				, include: "ApixServer" in nodeGroup) var nodeGroup = meta.const(initFlag[1])				, "MetaHeader" in nodeGroup[1] == statOperand[GroupTo-->stat];							}		} 	 } }using namespace userAuth.UITests.Droid.Protoneq.smash{			 #define charBind			 {				 private enum 			 }	}}
